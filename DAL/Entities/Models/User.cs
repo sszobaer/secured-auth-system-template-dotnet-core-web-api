01a42bbb14bf1c8fb7b258d070142c24a2640322
sszobaer <22-49415-3@student.aiub.edu>
2026-02-20 04:06:41 +0600

    Project Initailized and model added

---
+++ DAL/Entities/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DAL.Entities.Models
+{
+    public class User
+    {
+        [Required, Key]
+        public Guid UserId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        [ForeignKey("Role")]
+        public Guid RoleId { get; set; }    
+        public virtual UserRole Role { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ DAL/Entities/Models/UserRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities.Models
+{
+    public class UserRole
+    {
+        public Guid RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
